776c3f24821870464f7a98cd0c6d4931aeeb54b6
justine-tarlet <50885825+justine-tarlet@users.noreply.github.com>
2020-02-11 16:49:51 +0100

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -18,6 +18,8 @@ export default function Home() {
     </Hero>
     <Services/>
     <FeaturedRooms />
+        
+        <div>Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
 
     </>
     );
